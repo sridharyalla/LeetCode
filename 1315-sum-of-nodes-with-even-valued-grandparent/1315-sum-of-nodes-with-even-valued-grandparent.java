e91d82965af9c13948d3cb0bf13601edaad3f8ed
sridharyalla <Sridhar2007@gmail.com>
2022-02-14 18:56:50 +0530

    Time: 2 ms (71.45%), Space: 53.6 MB (5.03%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumEvenGrandparent(TreeNode root) {
+        
+        if( root == null)
+            return 0;
+        
+        
+        int sum=0;
+         if ( root.left != null){
+             if(root.val %2 == 0 && root.left.left != null)
+                 sum += root.left.left.val;
+             if(root.val %2 == 0 && root.left.right != null)
+                 sum += root.left.right.val;
+         }
+          if ( root.right != null){
+             if(root.val %2 == 0 && root.right.left != null)
+                  sum += root.right.left.val;
+             if(root.val %2 == 0 && root.right.right != null)
+                  sum += root.right.right.val;
+         }
+        
+        return sum + sumEvenGrandparent(root.left) + sumEvenGrandparent(root.right);
+    }
+}
