d10fec9b369850ffafeaf9d5b64021b299a6edfe
sridharyalla <Sridhar2007@gmail.com>
2022-02-18 12:02:20 +0530

    Time: 11 ms (32.76%), Space: 62.4 MB (18.57%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,7 +1,36 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         boolean[] visited = new boolean[arr.length];
-        return dfs(arr, start, visited);
+        return bfs(arr,start);
+        //return dfs(arr, start, visited);
+    }
+    
+    boolean bfs( int[] arr, int pos) {
+        
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[arr.length];
+        q.add(pos);
+        
+        while ( !q.isEmpty()){
+            
+            int node = q.poll();
+            
+            if( node < 0 || node >= arr.length)
+                continue;
+                        
+            if(arr[node] == 0)
+                return true;
+            
+            if( visited[node])
+                continue;
+            
+            visited[node] = true;
+            
+            q.add(node + arr[node]);
+            q.add(node - arr[node]);
+        }
+        
+        return false;
     }
     
     boolean dfs (int[] arr, int pos, boolean[] visited) {
