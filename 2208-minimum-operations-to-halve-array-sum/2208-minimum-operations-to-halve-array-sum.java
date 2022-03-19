d726a939385a60a6fc117052395ffac2845e408d
sridharyalla <Sridhar2007@gmail.com>
2022-03-19 23:24:03 +0530

    Time: 420 ms (20.00%), Space: 109.4 MB (20.00%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int halveArray(int[] nums) {
+        double sum = 0.0;
+        PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int n: nums){
+            sum += n;
+            pq.offer((double)n);
+        }
+        double target = sum/2;
+        int result = 0;
+        while( sum > target){
+            double val = pq.poll()/2;
+            sum -= val;
+            pq.offer(val);
+            result++;
+        }
+        return result;
+    }
+}
