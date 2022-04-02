76ad014861950a0a2f414bcb79ff5b3b8829305b
sridharyalla <Sridhar2007@gmail.com>
2022-04-02 23:01:26 +0530

    Time: 2 ms (97.65%), Space: 42.3 MB (96.48%) - LeetHub

--- 1087-brace-expansion/1087-brace-expansion.java
+++ 1087-brace-expansion/1087-brace-expansion.java
@@ -1,41 +1,46 @@
 class Solution {
-    
-    public String[] expand(String s) {
-        List<String> input = new ArrayList<>();
-        boolean expand = false;
-        String buffer = "";
-        for(int i=0; i < s.length(); i++){
-            if(s.charAt(i) == '{') {
-                expand = true;
-            } else if ( expand && s.charAt(i) >= 'a' &&  s.charAt(i) <= 'z') {
-                buffer += s.charAt(i);
-            } else if(!expand && s.charAt(i) >= 'a' &&  s.charAt(i) <= 'z') {
-                input.add(""+s.charAt(i));
-            } else if (s.charAt(i) == '}'){
-                input.add(buffer);
-                buffer = "";
-                expand = false;
+    int storeFirstOptions(String s, int startPos, List<Character> firstOptions) {
+        // If the first character is not '{', it means a single character
+        if (s.charAt(startPos) != '{') {
+            firstOptions.add(s.charAt(startPos));
+        } else {
+            // Store all the characters between '{' and '}'
+            while (s.charAt(startPos) != '}') {
+                 if (s.charAt(startPos) >= 'a' && s.charAt(startPos) <= 'z') {
+                     firstOptions.add(s.charAt(startPos));
+                 }
+                startPos++;
             }
+            // Sort the list
+            Collections.sort(firstOptions);
         }
-        Set<String> result = new TreeSet<>();
-        StringBuffer sb = new StringBuffer();
-        backtrack(result,0,sb,input);
-        String[] ret = new String[result.size()];
-        int pos =0;
-        for(String r : result)
-            ret[pos++] = r;
-        return ret;
+        // Increment it to point to the next character to be considered
+        return startPos + 1;
     }
     
-    void backtrack(Set<String> result, int pos, StringBuffer sb, List<String> input) {
-        String s = input.get(pos);
-        for(int j =0; j < s.length(); j++){
-            sb.append(s.charAt(j));
-            if( pos == input.size()-1){
-                result.add(sb.toString());
-            } else 
-                backtrack(result,pos+1,sb,input);
-            sb.deleteCharAt(sb.length()-1);
+    String[] findAllWords(String s, int startPos) {
+        // Return empty string list if the string is empty
+        if (startPos == s.length()) {
+            return new String[] {""};
         }
+        
+        List<Character> firstOptions = new ArrayList<>();
+        // Store the characters for the first index as string in firstOptions
+        int remStringStartPos = storeFirstOptions(s, startPos, firstOptions);
+        String[] wordsWithRemString = findAllWords(s, remStringStartPos);
+        
+        List<String> expandedWords = new ArrayList<>();
+        // Create new words by adding the character at the beginning
+        for (Character c : firstOptions) {
+            for (String word : wordsWithRemString) {
+                expandedWords.add(c + word);
+            }
+        }
+        
+        return expandedWords.toArray(new String[0]);
+    }
+    
+    public String[] expand(String s) {
+        return findAllWords(s, 0);
     }
 }
