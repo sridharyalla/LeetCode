48102a6e409af751b9494239730ebe53dfb011d4
sridharyalla <Sridhar2007@gmail.com>
2022-04-16 21:50:49 +0530

    Time: 12 ms (100.00%), Space: 39 MB (100.00%) - LeetHub

---
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+    []             [ ]
+     total/cost1    total -
+    **/
+    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        if( cost2 > cost1){
+            return waysToBuyPensPencils( total, cost2, cost1);
+        }
+        if( cost1 > total && cost2 >total)
+            return 1;
+        
+        int n = total/cost1;
+        long ans = 0;
+        for(int i = 0;i <=  n; i++){
+            int t = total-(cost1*i);
+            ans = ans + (t/cost2)+1; 
+
+        }
+        
+        return ans;
+    }
+}
